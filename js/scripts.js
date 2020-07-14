0d8e9d349d6532683e24e7401b63ddef39baa42b
patel108 <patel135lol@gmail.com>
2020-07-13 20:00:24 -0400

    Update scripts.js
    removed existing cose and added 3 list of pokemon objets

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-
-  alert('Hello world');
-  var favoriteFood = 'taco';
-  document.write(favoriteFood);
+var repository = [
+{
+  pokemonName : 'Butterfree',
+  pokemonHeight : 1.01,
+  pokemoneTypes : ['Bug'],
+},
+{
+  pokemonName : 'Rapidash',
+  pokemonHeight : 1.07,
+  pokemonTypes : ['Field'],
+},
+{
+  pokemonName : 'Genesect',
+  pokemonHeight : 1.05,
+  pokemonTypes : ['No Eggs'],
+},
+{
+  pokemonName : 'Pikachu',
+  pokemonHeight : 0.4,
+  pokemonTypes : ['Field, Fairy'],
+}
+]
+console.log(repository)
